dc0e3f33fe8e0dd122fe6484158f6ec731b2db94
lenayak <113600536+lenayak@users.noreply.github.com>
2022-12-12 11:16:16 +0400

    get next exemplar

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 import first_script
 import second_script
 import third_script
+import fourth_script
 
 
 if __name__ == "__main__":
@@ -15,3 +16,9 @@ if __name__ == "__main__":
     copy_dataset_path = os.path.abspath("../application_programming_l2/copy_dataset")
     path_to_files1 = third_script.get_path_to_files(copy_dataset_path)
     third_script.write_as_csv(copy_dataset_path, path_to_files1)
+    mark = 4
+    path_to_folder = path + f'/{mark}'
+    num_of_files = sum(os.path.isfile(os.path.join(path_to_folder, f))
+                        for f in os.listdir(path_to_folder)) + 1
+    for id in range(1,num_of_files):
+        print(fourth_script.get_next_exemplar(mark, id))
